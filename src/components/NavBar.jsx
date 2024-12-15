53ab4c3daeac2238926c66bdf0fea332d4c0734f
Aaron Vazquez <a_vazquez2@mail.fhsu.edu>
2024-12-15 14:57:53 -0600

    inital commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      NavBar
+      <div>
+        <h1>React Books App</h1>
+      </div>
+      <div>Your Favorites</div>
+    </div>
+  );
+};
+
+export default NavBar;
